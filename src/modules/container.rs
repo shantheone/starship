07b0547876daec634ff29b6eb0b078b551712357
shan <shandoor@gmail.com>
2025-10-25 13:29:36 +0200

    fix(modules): prompt shows Systemd when running in LXC container on Proxmox

--- src/modules/container.rs
+++ src/modules/container.rs
@@ -58,11 +58,12 @@ pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
 
         // WSL with systemd will set the contents of this file to "wsl"
         // Avoid showing the container module in that case
-        // Honor the contents of this file if "docker" and not running in podman or wsl
+        // Honor the contents of this file if "docker" or "lxc" and not running in podman or wsl
         let systemd_path = context_path(context, "/run/systemd/container");
         if let Ok(s) = utils::read_file(systemd_path) {
             match s.trim() {
                 "docker" => return Some("Docker".into()),
+                "lxc" => return Some("LXC".into()),
                 "wsl" => (),
                 _ => return Some("Systemd".into()),
             }
@@ -272,6 +273,17 @@ mod tests {
     #[cfg(target_os = "linux")]
     fn test_containerenv_docker_in_systemd() -> std::io::Result<()> {
         let (actual, expected) = containerenv_systemd(Some("docker"), Some("Docker"))?;
+
+        // Assert that the actual and expected values are the same
+        assert_eq!(actual, expected);
+
+        Ok(())
+    }
+
+    #[test]
+    #[cfg(target_os = "linux")]
+    fn test_containerenv_lxc_in_systemd() -> std::io::Result<()> {
+        let (actual, expected) = containerenv_systemd(Some("lxc"), Some("LXC"))?;
 
         // Assert that the actual and expected values are the same
         assert_eq!(actual, expected);
